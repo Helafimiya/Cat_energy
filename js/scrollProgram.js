4deb838caafd6249157eecce7a5f6310fea99448
Helafimiya <82267281+Helafimiya@users.noreply.github.com>
2022-01-16 16:35:17 +0300

    deleted

--- js/scrollProgram.js
+++
@@ -1,8 +0,0 @@
-const program = document.querySelector(".total-program");
-const buttonSearchProgram = document.querySelector(".promo__link");
-
-function handleButtonClick() {
-   program.scrollIntoView({block: "center", behavior: "smooth"});
-};
-
-buttonSearchProgram.addEventListener('click', handleButtonClick);
